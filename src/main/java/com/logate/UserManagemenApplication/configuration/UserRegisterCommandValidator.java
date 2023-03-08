472b34d1c9ed55972933143cd2368a731982249e
MarjanB00 <96199662+MarjanB00@users.noreply.github.com>
2023-03-08 17:40:50 +0100

    fixed validation

--- src/main/java/com/logate/UserManagemenApplication/configuration/UserRegisterCommandValidator.java
+++ src/main/java/com/logate/UserManagemenApplication/configuration/UserRegisterCommandValidator.java
@@ -3,10 +3,14 @@ package com.logate.UserManagemenApplication.configuration;
 import com.logate.UserManagemenApplication.dto.UserRegisterDTO;
 import com.logate.UserManagemenApplication.service.UserService;
 
+import liquibase.util.StringUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 @RequiredArgsConstructor
@@ -25,14 +29,85 @@ public class UserRegisterCommandValidator implements Validator {
         UserRegisterDTO userRegister = (UserRegisterDTO) target;
 
         validateEmail(userRegister.getEmail(), errors);
-
+        validateUsername(userRegister.getUsername(), errors);
+        validatePassword(userRegister.getPassword(), errors);
 
     }
 
     private void validateEmail(String email, Errors errors) {
         boolean doesUserWithEmailExists= userService.existsByEmail(email);
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (!matcher.matches()){
+            errors.rejectValue("email", "email.not-valid", "Email " + email + " is not a valid email address");
+            return;
+        }
+        if (email.chars().filter(chars -> chars == '@').count()!=1){
+            errors.rejectValue("email", "email.not-valid", "Email " + " must contain only one @ character" );
+            return;
+        }
         if(doesUserWithEmailExists){
             errors.rejectValue("email", "email.already-exists", "User with email " + email + " already exists!");
         }
+
     }
+
+    private void validateUsername(String username, Errors errors){
+        String numRegex   = ".*[0-9].*";
+        String alphaRegex = ".*[A-Z].*";
+        String smallalphaRegex = ".*[a-z].*";
+
+        char[] chars = username.toCharArray();
+        for (char c: chars
+             ) {
+            boolean a = String.valueOf(c).matches(numRegex);
+            boolean b = String.valueOf(c).matches(alphaRegex);
+            boolean d = String.valueOf(c).matches(smallalphaRegex);
+            boolean e = String.valueOf(c).matches("\\.");
+            if (!a && !b && !d && !e){
+                errors.rejectValue("username", "username.not-valid", "username " + " contains illegal characters");
+                return;
+            }
+        }
+    }
+
+    private void validatePassword(String password, Errors errors){
+        String smallalphaRegex = ".*[a-z].*";
+        String alphaRegex = ".*[A-Z].*";
+        String numRegex   = ".*[0-9].*";
+        Pattern special = Pattern.compile ("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+        Matcher hasSpecial = special.matcher(password);
+
+        if (password == null){
+            errors.rejectValue("password", "password.required", "password is required");
+            return;
+        }
+        if (password.trim().equals("")){
+            errors.rejectValue("password","password.empty", "Password is empty");
+        }
+
+        if (password.length()<8){
+            errors.rejectValue("password", "password.length-not-valid", "password is too short");
+            return;
+            }
+        if(!password.matches(smallalphaRegex)){
+            errors.rejectValue("password", "password.small-letters-not-found", "password must contain small letters");
+            return;
+        }
+        if (!password.matches(alphaRegex)){
+            errors.rejectValue("password", "password.capital-letters-not-found", "password must contain capital letters");
+            return;
+        }
+        if (!password.matches(numRegex)){
+            errors.rejectValue("password", "password.numbers-not-found", "password must contain numbers");
+            return;
+        }
+        if (!hasSpecial.find()){
+            errors.rejectValue("password", "password.spacial-car-not-found", "password must contain special character");
+        }
+
+    }
+
 }
--- src/test/java/com/logate/UserManagemenApplication/UserManagemenApplicationTests.java
+++ src/test/java/com/logate/UserManagemenApplication/UserManagemenApplicationTests.java
@@ -3,11 +3,21 @@ package com.logate.UserManagemenApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @SpringBootTest
 class UserManagemenApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+		Pattern pattern = Pattern.compile(regex);
+		String email="asd@SADas.com";
+		Matcher matcher = pattern.matcher(email);
+		System.out.println(matcher.matches());
+		String email2 = "as#d@SAD#as.com";
+		Matcher matcher2 = pattern.matcher(email2);
+		System.out.println(matcher2.matches());
 	}
-
 }
